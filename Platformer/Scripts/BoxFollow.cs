d42664b5a2172bd4f32aaca9b98ccc563754d8e1
tjsnyd <tjsnyder@calpoly.edu>
2015-08-06 16:07:40 -0700

    Create BoxFollow.cs

---
+++ Platformer/Scripts/BoxFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class BoxFollow : MonoBehaviour {
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+	}
+}
